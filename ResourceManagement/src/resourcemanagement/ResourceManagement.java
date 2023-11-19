4cff359971793e7bc8152717a51d1cffaf81d8f4
Thato <t.chitja369@gmail.com>
2023-11-19 14:06:21 +0200

    Add Connection String

--- ResourceManagement/src/resourcemanagement/ResourceManagement.java
+++ ResourceManagement/src/resourcemanagement/ResourceManagement.java
@@ -4,17 +4,46 @@
  */
 package resourcemanagement;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author tchit
  */
 public class ResourceManagement {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    public static Connection getConnection(){
+        Connection connection = null;
+            
+        try{
+            // Load the JDBC driver
+            Class.forName("org.sqlite.JDBC");
+            String url = "jdbc:sqlite:C:\\Users\\ic westgate\\Documents\\SQL Lite Programmes\\Task Manager.db\"";
+            
+            // Establish the connection
+            connection = DriverManager.getConnection(url);
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+        
+        return connection;
+    };
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Connection connection = ResourceManagement.getConnection();
+        
+        if (connection != null){
+            System.out.println("Connection to database established.");
+            try {
+                connection.close();
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Failed to connect to database");
+        }
     }
     
 }
